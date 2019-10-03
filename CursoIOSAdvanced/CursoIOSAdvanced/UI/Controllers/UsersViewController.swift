9c487418e03829e8ccdda1c0c7226cd9ed9bc74a
David Jardon <d.jardon@gmail.com>
2019-10-03 20:21:48 +0200

    Add icons

--- CursoIOSAdvanced/CursoIOSAdvanced/UI/Controllers/UsersViewController.swift
+++ CursoIOSAdvanced/CursoIOSAdvanced/UI/Controllers/UsersViewController.swift
@@ -11,7 +11,77 @@ import UIKit
 
 class UsersViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Properties
+    private var cellSpacing: CGFloat = 16.0
+    
+    
+    // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+
+// MARK: - Extension TableView methods
+extension UsersViewController: UITableViewDataSource, UITableViewDelegate {
+    /// Configure tableView with default options
+    func configure(tableView: UITableView) {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PersonTableViewCell.cellIdentifier,
+                                                       for: indexPath) as? PersonTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+}
+
+
+// MARK: - Extension CollectionView methods
+extension UsersViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    /// Configure collectionView with default options
+    func configure(collectionView: UICollectionView) {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PersonCollectionViewCell.cellIdentifier,
+                                                       for: indexPath) as? PersonCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+    
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let size = (collectionView.frame.size.width - cellSpacing) / 2
+        return CGSize(width: size,
+                      height: size)
     }
 }
