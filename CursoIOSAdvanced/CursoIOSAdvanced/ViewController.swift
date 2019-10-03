f02af800d510fce0702dc8e58549c21f1824b311
David Jardon <d.jardon@gmail.com>
2019-10-03 16:26:35 +0200

    Init project with Cocoapods

---
+++ CursoIOSAdvanced/CursoIOSAdvanced/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CursoIOSAdvanced
+//
+//  Created by David Jardon on 02/10/2019.
+//  Copyright © 2019 David Jardon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
