f99a796ac014b905cc351fc262b94a06bbda50e0
David Jardon <d.jardon@gmail.com>
2019-10-04 17:41:14 +0200

    Add lottie animation splash view

--- CursoIOSAdvanced/CursoIOSAdvanced/UI/Controllers/SplashViewController.swift
+++ CursoIOSAdvanced/CursoIOSAdvanced/UI/Controllers/SplashViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import Lottie
+
 
 class SplashViewController: UIViewController {
 
+    @IBOutlet weak var mViewLoading: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        playAnimation()
+        navigate()
+    }
+    
+    func playAnimation() {
+        let animationView = AnimationView(name: "loading_animation")
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.frame = mViewLoading.bounds
+        mViewLoading.addSubview(animationView)
         
+        animationView.play()
+    }
+
+
+    private func navigate() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             
@@ -25,7 +45,5 @@ class SplashViewController: UIViewController {
             self.present(viewControllerDestination, animated: true)
         }
     }
-
-
 }
 
