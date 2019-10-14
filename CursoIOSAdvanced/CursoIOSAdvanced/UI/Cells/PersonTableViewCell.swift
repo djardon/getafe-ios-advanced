7e057030ed350e03aa03069ee318a46b0be6fa99
David Jardon <d.jardon@gmail.com>
2019-10-14 20:54:45 +0200

    Add RefreshControll and code improvements

--- CursoIOSAdvanced/CursoIOSAdvanced/UI/Cells/PersonTableViewCell.swift
+++ CursoIOSAdvanced/CursoIOSAdvanced/UI/Cells/PersonTableViewCell.swift
@@ -17,12 +17,14 @@ class PersonTableViewCell: UITableViewCell {
     @IBOutlet weak var mImage: UIImageView!
     @IBOutlet weak var mLabelName: UILabel!
     @IBOutlet weak var mLabelEmail: UILabel!
+    @IBOutlet weak var mLabelBirthdate: UILabel!
     
     
     override func prepareForReuse() {
         mImage.image = nil
         mLabelName.text = nil
         mLabelEmail.text = nil
+        mLabelBirthdate.text = nil
     }
     
     override func awakeFromNib() {
@@ -32,10 +34,16 @@ class PersonTableViewCell: UITableViewCell {
         mView.configureShadows()
     }
     
-    func configureCell(image: String? = nil, name: String? = nil, subtitle: String? = nil) {
+    func configureCell(image: String? = nil, name: String? = nil, subtitle: String? = nil, birthdate: Date? = nil) {
         let url = URL(string: image ?? "")
         mImage.kf.setImage(with: url)
         mLabelName.text = name
         mLabelEmail.text = subtitle
+        
+        if let birthdateValue = birthdate {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            mLabelBirthdate.text = dateFormatter.string(from: birthdateValue)
+        }
     }
 }
